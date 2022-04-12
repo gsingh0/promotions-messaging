8b89d9543893ff0d82c1ea3707cf866798414f53
Gurnoor Singh <gurnoorsingh@Gurnoors-MacBook-Pro.local>
2022-04-11 20:15:31 -0400

    okta configs

--- api-gateway-service/src/main/java/com/vmware/api/gateway/service/configurations/OktaOAuth2WebSecurityConfigurerAdapter.java
+++ api-gateway-service/src/main/java/com/vmware/api/gateway/service/configurations/OktaOAuth2WebSecurityConfigurerAdapter.java
@@ -1,19 +1,19 @@
 package com.vmware.api.gateway.service.configurations;
 
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-//@Configuration
-//public class OktaOAuth2WebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
-//
-//    @Override
-//    public void configure(HttpSecurity httpSecurity) throws Exception {
-//        httpSecurity
-//                .authorizeRequests().anyRequest().authenticated()
-//                    .and()
-//                .oauth2Login()
-//                    .and()
-//                .oauth2ResourceServer().jwt();
-//    }
-//}
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class OktaOAuth2WebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .authorizeRequests().anyRequest().authenticated()
+                    .and()
+                .oauth2Login()
+                    .and()
+                .oauth2ResourceServer().jwt();
+    }
+}
--- messaging-service/src/main/java/com/vmware/messaging/service/configurations/OktaOAuth2WebSecurityConfigurerAdapter.java
+++ messaging-service/src/main/java/com/vmware/messaging/service/configurations/OktaOAuth2WebSecurityConfigurerAdapter.java
@@ -1,17 +1,17 @@
 package com.vmware.messaging.service.configurations;
 
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-//@Configuration
-//public class OktaOAuth2WebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//            .authorizeRequests().anyRequest().authenticated()
-//                .and()
-//            .oauth2ResourceServer().jwt();
-//    }
-//}
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class OktaOAuth2WebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests().anyRequest().authenticated()
+                .and()
+            .oauth2ResourceServer().jwt();
+    }
+}

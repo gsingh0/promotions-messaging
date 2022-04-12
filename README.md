7e9627122689eddfbb9f8db9a36c31dd47648dba
Gurnoor Singh <gurnoorsingh@Gurnoors-MacBook-Pro.local>
2022-04-11 23:04:23 -0400

    implemented basic auth

--- README.md
+++ README.md
@@ -6,4 +6,18 @@
 - when talking about scale
   - mention containerizing each service and having multiple instances of each
   - mention that when theres a lot of emails, then you can integrate batch processing via GKE batch job
-  - mention 
+
+- security
+  - simple security implementation defined
+    - using basic auth assuming that there is an authentication resource that has username and passwords of all the users
+    - for now use a single test user
+    - in enterprise environment, need to use oauth or ldap authentication
+      - oauth2:
+        - client send authorization request to resource owner (user)
+        - gets back an authorization grant
+        - client sends auth grant to authorization server
+        - gets back a token
+        - client can use token to access resource server
+        - get back resource data
+
+
--- api-gateway-service/src/main/java/com/vmware/api/gateway/service/configurations/SecurityApplicationAdapter.java
+++ api-gateway-service/src/main/java/com/vmware/api/gateway/service/configurations/SecurityApplicationAdapter.java
@@ -1,19 +1,22 @@
 package com.vmware.api.gateway.service.configurations;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-public class OktaOAuth2WebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity
+public class SecurityApplicationAdapter extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
+                .csrf().disable()
                 .authorizeRequests().anyRequest().authenticated()
-                    .and()
-                .oauth2Login()
-                    .and()
-                .oauth2ResourceServer().jwt();
+                .and()
+                .httpBasic();
     }
 }
